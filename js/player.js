b3900c8fc9bc397a0d97e3d4174361aad764f5f0
dmlgzs <dmlgzs@qq.com>
2022-12-29 17:27:37 +0800

    fix: solve the garbled code

--- js/player.js
+++ js/player.js
@@ -28,25 +28,8 @@ var hashLock = false
 var dragTarget = null
 
 function bytesToString(arr) {
-	if (typeof arr === 'string') {
-		return arr;
-	}
-	var str = '', _arr = arr;
-	for (var i = 0; i < _arr.length; i++) {
-		var one = _arr[i].toString(2), v = one.match(/^1+?(?=0)/);
-		if (v && one.length == 8) {
-			var bytesLength = v[0].length;
-			var store = _arr[i].toString(2).slice(7 - bytesLength);
-			for (var st = 1; st < bytesLength; st++) {
-				store += _arr[st + i].toString(2).slice(2);
-			}
-			str += String.fromCharCode(parseInt(store, 2));
-			i += bytesLength - 1;
-		} else {
-			str += String.fromCharCode(_arr[i]);
-		}
-	}
-	return str;
+	var decoder = new TextDecoder("utf-8")
+	return decoder.decode(new Uint8Array(arr))
 }
 
 function corsBridge(input){
